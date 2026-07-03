cb0665e4d7fb8caca9b9db4eb51806c60d837962
Saad Ali <saadalieditor6@gmail.com>
2026-07-03 21:05:23 +0500

    Add LoginPromptModal to App component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import Sidebar from './components/Sidebar/Sidebar.jsx';
 import ChatArea from './components/ChatArea/ChatArea.jsx';
 import LimitModal from './components/Modals/LimitModal.jsx';
+import LoginPromptModal from './components/Modals/LoginPromptModal.jsx';
 import { useAppContext } from './context/AppContext.jsx';
 import './styles/App.css';
 
@@ -43,6 +44,7 @@ export default function App() {
       {limitModal && (
         <LimitModal payload={limitModal} onClose={() => setLimitModal(null)} />
       )}
+      <LoginPromptModal />
     </div>
   );
 }
